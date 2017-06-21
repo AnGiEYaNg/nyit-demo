9c2560692777819aba42f85a3c96cb57a4f53d92
Angie <angi3yang@gmail.com>
2017-06-20 23:50:29 -0400

    add in master and bachelor pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,14 @@
 angular.module('main', ['ui.router'])
-.component('headerCompt', {
-	template: '<h1>Header</h1>'
+.controller('bachelorsLandingCtrl', function($scope){
+	$scope.images = [
+		"assets/BFA - All/Copy of COAS_FineArts_BFAStills_003.png",
+		"assets/BFA - All/Copy of COAS_FineArts_BFAStills_004.png",
+		"assets/BFA - All/Copy of COAS_FineArts_BFAStills_005.jpg",
+		"assets/BFA - All/Copy of COAS_FineArts_BFAStills_005.png",
+		"assets/BFA - All/Copy of COAS_FineArts_BFAStills_008.jpg",
+		"assets/BFA - All/Copy of COAS_FineArts_BFAStills_008.png",
+		"assets/BFA - All/Copy of COAS_FineArts_BFAStills_011.png",
+		"assets/BFA - All/Copy of COAS_FineArts_BFAStills_013.png",
+		"assets/BFA - All/Copy of COAS_FineArts_BFAStills_014.png"
+	]
 });
--- src/js/routes.js
+++ src/js/routes.js
@@ -4,6 +4,36 @@ angular.module('main')
 	.state({
 		name: 'home',
 		url: '',
-		templateUrl: 'homepage.html'
+		templateUrl: 'homepage.html',
+		data: {
+			breadcrumb: 'Home'
+		}
+	})
+	.state({
+		name: 'mastersLanding',
+		url: '/masters_of_fine_art',
+		templateUrl: 'mastersLanding.html',
+		data: {
+			breadcrumb: 'Masters of Fine Art'
+		}
+	})
+	.state({
+		name: 'bachelorsLanding',
+		url: '/bachelors_of_fine_art',
+		templateUrl: 'bachelorsLanding.html',
+		controller: 'bachelorsLandingCtrl',
+		data: {
+			breadcrumb: 'Bachelors of Fine Art'
+		}
+	})
+	.state({
+		name: 'subsection',
+		url: '/:subsection',
+		templateUrl: 'subsection.html',
+	})
+	.state({
+		name: 'credits',
+		url: '/credits',
+		templateUrl: 'credits.html',
 	})
 })
