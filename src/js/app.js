dc5b2f4c7df8012308971fef7a6b89b33acf1b5a
Angie <angi3yang@gmail.com>
2017-06-20 21:46:59 -0400

    add in header

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+angular.module('main', ['ui.router'])
+.component('headerCompt', {
+	template: '<h1>Header</h1>'
+});
---
+++ src/js/routes.js
@@ -0,0 +1,9 @@
+angular.module('main')
+.config(function($stateProvider){
+	$stateProvider
+	.state({
+		name: 'home',
+		url: '',
+		templateUrl: 'homepage.html'
+	})
+})
